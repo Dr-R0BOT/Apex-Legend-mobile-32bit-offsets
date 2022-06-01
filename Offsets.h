1be5d1bc8c6e51c2719544dfb37b12d913fff221
Dr-R0BOT <93642070+Dr-R0BOT@users.noreply.github.com>
2022-06-01 07:41:24 -0700

    x
    x

---
+++ Offsets.h
@@ -0,0 +1,31 @@
+#pragma once
+
+
+namespace Offset
+{
+	const DWORD GWorld = 0x6512120;
+	const DWORD GName = 0x67D7900;
+
+	const DWORD PersistentLevel = 0x20;
+	const DWORD NetDriver = 0x24;
+	const DWORD ServerConnection = 0x70;
+	const DWORD AcknowledgedPawn = 0x26c;
+	const DWORD EntityList = 0x30c
+	const DWORD ApexPlayerController = 0x48;
+	const DWORD RootComponent = 0x11c
+	const DWORD TeamID = 0x1dc
+	const DWORD PoseState = 0x2dc
+	const DWORD isBot = 420;
+	const DWORD ItemBase = 0xa8
+	const DWORD Velocity = 0x18;
+	const DWORD PickUpDataList = 0x3c;
+	const DWORD CharacterMovement = 0x214;
+	const DWORD RelativeLocation = 0x2c;
+	const DWORD RelativeRotation = 0x38;
+	const DWORD LineOfSightTo = 0x4684b4c
+	const DWORD SkeletalMeshComponent = 0x214;
+	const DWORD ParachuteComponent = 0x1098;
+	const DWORD PlayerCameraManager = 0x278;
+	const DWORD CameraCache = 0x240;
+
+}
